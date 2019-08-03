f9e07c433323d3d3435ac619f3d37d4ac3d8127c
Bartosz Rak <49698017+BartoszRak@users.noreply.github.com>
2019-08-03 05:04:04 +0200

    feat: Add typescript.

--- src/theme/withMuiTheme.tsx
+++ src/theme/withMuiTheme.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { CssBaseline, MuiThemeProvider, createMuiTheme, responsiveFontSizes } from '@material-ui/core'
 import theme from './theme'
 
-const withMuiTheme = (Component, withBaseline, additionalTheme) => {
+const withMuiTheme = (Component: any, withBaseline: any, additionalTheme?: any) => {
   const muiTheme = responsiveFontSizes(createMuiTheme(
     additionalTheme ? { ...theme, ...additionalTheme } : theme
   ), {})
   if (process.env.NODE_ENV !== 'production') {
     console.info('[THEME]', muiTheme)
   }
-  const HOC = componentProps => (
+  const HOC = (componentProps: any) => (
     <MuiThemeProvider theme={muiTheme}>
       {withBaseline && <CssBaseline />}
       <Component {...componentProps} />
